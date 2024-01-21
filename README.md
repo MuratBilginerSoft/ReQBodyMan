2a6731cc56cf9061efd0cca7f25aa7d06edd8697
mb_trendify <muratbilginer09@gmail.com>
2024-01-21 18:50:51 +0300

    v1.1.0 Completed

--- README.md
+++ README.md
@@ -34,43 +34,47 @@ pip3 install ReQBodyMan
 from ReQBodyMan.ReQBodyMan import ReQBodyMan
 ```
 
-**Note: Boolean values have a return value of 1 if True and a return value of 0 if False.**
-
 ### 2 Get Form Data
 
 If the data comes from the request with a form, you can get the data as follows.
 
 ```py
+
 Data : string, int, float, boolean, list, dict,  
-Parameters : contextName, variableType
+Parameters : contextName, variableType, booleanType=NoneRequired
+booleanType default = "int"
 Return: data
 
 data = self.ReQBodyMans.form("data_name", "str")
 data = self.ReQBodyMans.form("data_name", "int")
 data = self.ReQBodyMans.form("data_name", "float")
-data = self.ReQBodyMans.form("data_name", "bool")
+data = self.ReQBodyMans.form("data_name", "bool", booleanType="bool")
+data = self.ReQBodyMans.form("data_name", "bool", booleanType="int" )
 data = self.ReQBodyMans.form("data_name", "list")
 data = self.ReQBodyMans.form("data_name", "dict")
 ```
 
-If Variable Type is sent outside the specified format, data is received according to the str format.
+If Variable Type is sent outside the specified format, return None.
 
 ### 3 Get Json Data
 
 ```py
+
 Data : string, int, float, boolean, list, dict,  
-Parameters : contextName, variableType
+Parameters : contextName, variableType, booleanType=NoneRequired
+booleanType default = "int"
 Return : Data
 
 data = self.ReQBodyMans.json("data_name", "str")
 data = self.ReQBodyMans.json("data_name", "int")
 data = self.ReQBodyMans.json("data_name", "float")
-data = self.ReQBodyMans.json("data_name", "bool")
+data = self.ReQBodyMans.json("data_name", "bool", booleanType="bool")
+data = self.ReQBodyMans.json("data_name", "bool", booleanType="int")
 data = self.ReQBodyMans.json("data_name", "list")
 data = self.ReQBodyMans.json("data_name", "dict")
 ```
 
-If Variable Type is sent outside the specified format, data is received according to the str format.
+If Variable Type is sent outside the specified format, return None.
 
 ### 4 Get File
 
@@ -87,28 +91,28 @@ If FileName is not in request.file, the value "The {fileName} is not in request.
 
 ```py
 Data : string, int, float, boolean, list,  
-Parameters : variableName, variableType
+Parameters : variableName, variableType, booleanType=NoneRequired
+booleanType default = "int"
 Return : data
 
 data = self.ReQBodyMans.params("data_name", "str")
 data = self.ReQBodyMans.params("data_name", "int")
 data = self.ReQBodyMans.params("data_name", "float")
-data = self.ReQBodyMans.params("data_name", "bool")
+data = self.ReQBodyMans.params("data_name", "bool", booleanType="bool")
+data = self.ReQBodyMans.params("data_name", "bool", booleanType="int")
 data = self.ReQBodyMans.params("data_name", "list")
 ```
 
-If variablename is sent outside the specified format,
-
-variableType : str return : ""
-variableType : int return : 0
-variableType : float return : 0.0
-variableType : bool return : 0
-variableType : list return : []
-
-
-
+If Variable Type is sent outside the specified format, return None.
 
 # Release Note
+
+## v.1.1.0
+
+1. Added dict property to Params.
+2. Added dict property to Json
+3. The return value for bool type has been added to return int or bool type according to the value to be given with booleanType.
+4. Code refactor.
 
 ## v.1.0.1
 
--- src/ReQBodyMan/ReQBodyMan.py
+++ src/ReQBodyMan/ReQBodyMan.py
@@ -6,44 +6,28 @@ import ast
 
 # endregion
 
-# region RequestContent Class
-
 class ReQBodyMan():
-
-    # region Init
-
-    def __init__(self):
-        pass
-
-    # endregion
-
-    # region Logix
 
     # region Form
 
-    def form(self, contentText, variableType):
-
-        # region Get Form Content
+    def form(self, contentText, variableType, booleanType="int"):
 
         self.__formContent = request.form
 
-        # endregion
-
-        # region Such A ContentText
-
         if contentText in self.__formContent:
 
-            # region Int
+            if variableType == "str":
+                    
+                if self.__formContent[contentText]:
+                    return str(self.__formContent[contentText]).strip()
+                else:
+                    return ""
 
-            if variableType == "int":
+            elif variableType == "int":
                 if self.__formContent[contentText]:
                     return int(self.__formContent[contentText])
                 else:
                     return None
-
-            # endregion
-
-            # region Float
 
             elif variableType == "float":
 
@@ -52,46 +36,41 @@ class ReQBodyMan():
                 else:
                     return None
             
-            # endregion
+            elif variableType == "bool":
 
-            # region Str
-
-            elif variableType == "str":
+                if booleanType == "int":
                     
-                if self.__formContent[contentText]:
-                    return str(self.__formContent[contentText]).strip()
-                else:
-                    return ""
+                    if self.__formContent[contentText]:
+                        
+                        return 1 if self.__formContent[contentText] == True else 0
+                    
+                    else:
+                        return 0
+                    
+                elif booleanType == "bool":
 
-            # endregion
-                
-            # region List
-
+                    if self.__formContent[contentText]:
+                        
+                        return True if self.__formContent[contentText] == True else False
+                    
+                    else:
+                        return False
+            
             elif variableType == "list":
                     
                 if self.__formContent[contentText]:
                     return ast.literal_eval(self.__formContent[contentText]) 
                 else:
                     return []
-            
-            # endregion
-                
-            # region Dict
 
             elif variableType == "dict":
                     
                 if self.__formContent[contentText]:
 
-                    strDict = self.__formContent[contentText]
-                    jsonDict = json.loads(strDict)
-                    
-                    return jsonDict
-                else:
-                    return []
-            
-            # endregion
+                    return json.loads(self.__formContent[contentText])
 
-            # region Any
+                else:
+                    return None
 
             else:
                 
@@ -99,97 +78,76 @@ class ReQBodyMan():
                     return self.__formContent[contentText]
                 else:
                     return None
-            
-            # endregion
-
-        # endregion
-
-        # region Not Such A ContentText
-
+                 
         else:
             return None
         
-        # endregion
-
     # endregion
 
     # region Json
 
-    def json(self, contentText, variableType):
+    def json(self, contentText, variableType, booleanType="int"):
 
         self.__jsonContent = request.get_json()
 
-        # region Such A ContentText
-
         if contentText in self.__jsonContent:
 
-            # region Int
+            if variableType == "str":
 
-            if variableType == "int":
+                if self.__jsonContent[contentText]:
+                    return str(self.__jsonContent[contentText]).strip()
+                else:
+                    return None
+
+            elif variableType == "int":
 
                 if self.__jsonContent[contentText]:
                     return int(self.__jsonContent[contentText])
                 else:
                     return None
-            
-            # endregion
-
-            # region Float
 
             elif variableType == "float":
                     
                 if self.__jsonContent[contentText]:
                     return float(self.__jsonContent[contentText])
                 else:
-                    return 0
-                    
-            # endregion
-            
-            # region Str
-
-            elif variableType == "str":
-
-                if self.__jsonContent[contentText]:
-                    return str(self.__jsonContent[contentText]).strip()
-                else:
-                    return ""
-            
-            # endregion
-
-            # region Bool
+                    return None
 
             elif variableType == "bool":
+
+                if booleanType == "int":
                     
-                if self.__jsonContent[contentText]:
-                    return 1 if self.__jsonContent[contentText] == True else 0
-                else:
-                    return 0
+                    if self.__jsonContent[contentText]:
+                        
+                        return 1 if self.__jsonContent[contentText] == True else 0
+                    
+                    else:
+                        return None
+                    
+                elif booleanType == "bool":
 
-            # endregion
-
-            # region List
+                    if self.__jsonContent[contentText]:
+                        
+                        return True if self.__jsonContent[contentText] == True else False
+                    
+                    else:
+                        return None
 
             elif variableType == "list":
                     
                 if self.__jsonContent[contentText]:
                     return self.__jsonContent[contentText]
                 else:
-                    return []
-            
-            # endregion
-            
-            # region Dict
+                    return None
 
             elif variableType == "dict":
-                    
-                if self.__jsonContent[contentText]:
-                    return self.__jsonContent[contentText]
-                else:
-                    return {}
-                
-            # endregion
 
-            # region Any
+                if self.__jsonContent[contentText]:
+
+                    return json.loads(self.__jsonContent[contentText])
+                
+                else:
+                    return None
 
             else:
 
@@ -197,43 +155,13 @@ class ReQBodyMan():
                     return self.__jsonContent[contentText]
                 else:
                     return None
-            
-            # endregion
-
-        # endregion
-
-        # region Not Such A ContentText
 
         else:
 
-            if variableType == "int" or variableType == "float":
-
-                return None
-            
-            elif variableType == "str":
-                    
-                return ""
-            
-            elif variableType == "bool":
-
-                return 0
-            
-            elif variableType == "list":
-
-                return []
-            
-            elif variableType == "dict":
-
-                return {}
-            
-            else:
-
-                return None
-        
-        # endregion
+            return None
 
     # endregion
-
+        
     # region File
 
     def file(self, fileName):
@@ -242,14 +170,13 @@ class ReQBodyMan():
             file = request.files[fileName]
             return file
         else:
-            file = None
             return f"The {fileName} is not in request.files"
 
     # endregion
 
     # region Params
 
-    def params(self, variable, variableType):
+    def params(self, variable, variableType, booleanType="int"):
 
         if str(variable) in request.args:
 
@@ -260,30 +187,23 @@ class ReQBodyMan():
                 return int(request.args.get(variable))
 
             elif variableType == "float":
-
                 return float(request.args.get(variable))
             
             elif variableType == "bool":
 
-                return 1 if request.args.get(variable) == True else 0
+                if booleanType == "int":
+                    return 1 if request.args.get(variable) == True else 0
+                elif booleanType == "bool":
+                    return True if request.args.get(variable) == True else False
             
             elif variableType == "list":
-
                 return ast.literal_eval(request.args.get(variable))
             
-        else:
-            if variableType == "str":
-                return ""
-            elif variableType == "int":
-                return 0
-            elif variableType == "float":
-                return 0.0
-            elif variableType == "bool":
-                return 0
-            elif variableType == "list":
-                return []
             elif variableType == "dict":
-                return {}
+                return json.loads(request.args.get(variable))
+                    
+        else:
+            return None
         
     # endregion
 
